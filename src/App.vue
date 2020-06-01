d9c89694b68e0b32a49465ad7f2222dc6f85afd3
Felixkruemel <felix.kruemel@gmx.de>
2020-06-01 13:15:17 +0000

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navbar></navbar>
+    <home></home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import home from './components/home.vue'
+import navbar from './components/navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    home,
+    navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-dark justify-content-center">
+        <a class="navbar-brand" >Kruemel's Page</a>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" >Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" >Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" >Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  
+}
+</script>
+
+
+<style>
+
+</style>
